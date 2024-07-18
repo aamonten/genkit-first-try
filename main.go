b355d5d166bf1e9b22afbe09b7533a0975a8a36a
Alejandro Montenegro <amontene@google.com>
2024-07-18 13:31:56 +0200

    demoing function calling

--- main.go
+++ main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/firebase/genkit/go/ai"
-	"github.com/firebase/genkit/go/plugins/ollama"
+	"github.com/firebase/genkit/go/plugins/vertexai"
 )
 
 func main() {
 
 	ctx := context.Background()
 
-	if err := ollama.Init(ctx, "http://127.0.0.1:11434"); err != nil {
+	if err := vertexai.Init(ctx, nil); err != nil {
 		log.Fatal(err)
 	}
 
-	gemmaModel := ollama.DefineModel(
+	gemini15pro := vertexai.Model("gemini-1.5-pro")
+
+	/* if err := ollama.Init(ctx, "http://127.0.0.1:11434"); err != nil {
+		log.Fatal(err)
+	} */
+
+	/* 	gemmaModel := ollama.DefineModel(
 		ollama.ModelDefinition{
 			Name: "gemma2",
 			Type: "generate",
@@ -25,26 +30,40 @@ func main() {
 		&ai.ModelCapabilities{
 			Multiturn:  false,
 			SystemRole: true,
-			Tools:      false,
+			Tools:      true,
 			Media:      false,
+		},
+	) */
+
+	myJoke := &ai.ToolDefinition{
+		Name:        "myJoke",
+		Description: "useful when you need a joke to tell",
+		InputSchema: make(map[string]any),
+		OutputSchema: map[string]any{
+			"joke": "string",
+		},
+	}
+	ai.DefineTool(
+		myJoke,
+		nil,
+		func(ctx context.Context, input map[string]any) (map[string]any, error) {
+			return map[string]any{"joke": "haha Just kidding no joke! got you"}, nil
 		},
 	)
 
-	genRes, err := gemmaModel.Generate(ctx, ai.NewGenerateRequest(
-		nil, ai.NewUserTextMessage("tell me a joke.")),
-		func(ctx context.Context, grc *ai.GenerateResponseChunk) error {
-			text, err := grc.Text()
-			if err != nil {
-				return err
-			}
-			fmt.Printf("%s", text)
-			return nil
-		})
+	request := ai.GenerateRequest{
+		Messages: []*ai.Message{
+			{Content: []*ai.Part{ai.NewTextPart("Tell me a joke.")},
+				Role: ai.RoleUser},
+		},
+		Tools: []*ai.ToolDefinition{myJoke},
+	}
+	response, err := gemini15pro.Generate(ctx, &request, nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	print("Response:")
-	print(genRes.Text())
+	print(response.Text())
 }
